fdd7ce4e6e4f16e69f2c27c06017107cbf5dc22b
sasalastoveckij-a11y <sasalastoveckij@gmail.com>
2025-12-15 13:56:54 +0200

    Function signatures

---
+++ CinemaBookingSystemTests/CinemaBookingSystem.tests/Model/CinemaBookingSystem.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaBookingSystem.tests.Model;
+
+public class CinemaBookingSystem
+{
+    public int SitCount { get; set; }
+    public int BookedSitsCount { get; set; }
+
+    public void BookSit(int sitNumber) { }
+
+    public bool SitIsBooked(int sitNumber) => false;
+}
