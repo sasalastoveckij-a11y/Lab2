03f3a4f60042b92030c60eec727fabd1e3f1e7e1
sasalastoveckij-a11y <sasalastoveckij@gmail.com>
2025-12-15 15:35:40 +0200

    Added Docker support

---
+++ CinemaBookingSystemTests/CinemaBookingSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using CinemaBookingSystemTests.Model;
+
+var cinema = new CinemaBookingSystem(50);
+
+Console.WriteLine($"Booking sit 1 ...");
+cinema.BookSit(1);
+DisplayBookedSits();
+
+Console.WriteLine($"Booking sit 4 ...");
+cinema.BookSit(4);
+DisplayBookedSits();
+
+Console.WriteLine($"Booking sit 5 ...");
+cinema.BookSit(5);
+DisplayBookedSits();
+
+Console.WriteLine($"Unbooking sit 4 ...");
+cinema.CancelBooking(4);
+DisplayBookedSits();
+
+
+void DisplayBookedSits() 
+{
+    Console.Write("Booked Sits: ");
+    foreach (var sit in cinema.BookedSits)
+    {
+        Console.Write($"{sit}, ");
+    }
+    Console.WriteLine();
+}
